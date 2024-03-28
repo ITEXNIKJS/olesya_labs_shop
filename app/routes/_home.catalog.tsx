604d5b13c8e490a29149563f0740b4d8398937e8
TEXNIK <94114335+ITEXNIKJS@users.noreply.github.com>
2024-03-28 22:14:03 +0300

    statistics

--- app/routes/_home.catalog.tsx
+++ app/routes/_home.catalog.tsx
@@ -1,6 +1,12 @@
 import { products } from "@prisma/client";
 import { LoaderFunctionArgs, json } from "@remix-run/node";
-import { Form, useLoaderData, useNavigate, useSubmit } from "@remix-run/react";
+import {
+  Form,
+  Link,
+  useLoaderData,
+  useNavigate,
+  useSubmit,
+} from "@remix-run/react";
 import { FC, useEffect, useState } from "react";
 import { ProductCard } from "~/components/product";
 import { Button } from "~/components/ui/button";
@@ -81,118 +87,123 @@ const IndexHome: FC = () => {
 
   return (
     <>
+      <div className="flex flex-row">
+        <div className="flex flex-col gap-4 p-4 w-60 border-r border-r-slate-300">
+          <Form
+            id="search-form"
+            role="search"
+            className="flex flex-col gap-4 whitespace-nowrap w-full"
+            onChange={(event) => submit(event.currentTarget)}
+          >
+            <div className="flex flex-col gap-2">
+              <h3 className="text-base font-semibold">Модель устройства</h3>
+              <RadioGroup
+                id="model"
+                name="model"
+                role="search"
+                onValueChange={(value) => setModel(value)}
+                value={model}
+              >
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="Samsung" id="option-one" />
+                  <Label htmlFor="option-one">Samsung</Label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="Nothing" id="option-two" />
+                  <Label htmlFor="option-two">Nothing</Label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="realme" id="option-two" />
+                  <Label htmlFor="option-two">Realme</Label>
+                </div>
+              </RadioGroup>
+            </div>
+            <div className="flex flex-col gap-2">
+              <h3 className="text-base font-semibold flex flex-row justify-between">
+                <p>Мин цена</p>
+                <p>{price}</p>
+              </h3>
+              <Slider
+                id="price"
+                name="price"
+                role="search"
+                max={200000}
+                step={500}
+                onValueChange={(value) => setPrice(value[0].toString())}
+                value={[parseInt(price)]}
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <h3 className="text-base font-semibold flex flex-row justify-between">
+                Объём памяти
+              </h3>
+              <RadioGroup
+                id="ram"
+                name="ram"
+                role="search"
+                onValueChange={(value) => setRam(value)}
+                value={ram}
+              >
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="4" id="option-one" />
+                  <Label htmlFor="option-one">4</Label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="6" id="option-two" />
+                  <Label htmlFor="option-two">6</Label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="8" id="option-two" />
+                  <Label htmlFor="option-two">8</Label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="12" id="option-two" />
+                  <Label htmlFor="option-two">12</Label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="16" id="option-two" />
+                  <Label htmlFor="option-two">16</Label>
+                </div>
+              </RadioGroup>
+            </div>
+            <div className="flex flex-col gap-2">
+              <h3 className="text-base font-semibold flex flex-row justify-between">
+                Поиск
+              </h3>
+              <Input
+                type="text"
+                role="search"
+                id="q"
+                name="q"
+                placeholder="Поиск..."
+                onChange={(e) => setQ(e.target.value)}
+                value={q}
+              />
+            </div>
+            <Button
+              className="w-full"
+              type="reset"
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              Сбросить
+            </Button>
+          </Form>
+          <Button asChild>
+            <Link to="/statistics">Статистика продаж</Link>
+          </Button>
+        </div>
 
-    <div className="flex flex-row">
-      <Form
-        id="search-form"
-        role="search"
-        className="flex flex-col gap-4 border-r border-r-slate-300 p-4 whitespace-nowrap min-w-60"
-        onChange={(event) => submit(event.currentTarget)}
-      >
-        <div className="flex flex-col gap-2">
-          <h3 className="text-base font-semibold">Модель устройства</h3>
-          <RadioGroup
-            id="model"
-            name="model"
-            role="search"
-            onValueChange={(value) => setModel(value)}
-            value={model}
-          >
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="Samsung" id="option-one" />
-              <Label htmlFor="option-one">Samsung</Label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="Nothing" id="option-two" />
-              <Label htmlFor="option-two">Nothing</Label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="realme" id="option-two" />
-              <Label htmlFor="option-two">Realme</Label>
-            </div>
-          </RadioGroup>
+        <div className="p-4 flex flex-wrap gap-4">
+          {data.products.map((product) => (
+            <ProductCard
+              key={product.id}
+              {...(product as Omit<products, "created_at" | "updated_at">)}
+            />
+          ))}
         </div>
-        <div className="flex flex-col gap-2">
-          <h3 className="text-base font-semibold flex flex-row justify-between">
-            <p>Мин цена</p>
-            <p>{price}</p>
-          </h3>
-          <Slider
-            id="price"
-            name="price"
-            role="search"
-            max={200000}
-            step={500}
-            onValueChange={(value) => setPrice(value[0].toString())}
-            value={[parseInt(price)]}
-          />
-        </div>
-        <div className="flex flex-col gap-2">
-          <h3 className="text-base font-semibold flex flex-row justify-between">
-            Объём памяти
-          </h3>
-          <RadioGroup
-            id="ram"
-            name="ram"
-            role="search"
-            onValueChange={(value) => setRam(value)}
-            value={ram}
-          >
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="4" id="option-one" />
-              <Label htmlFor="option-one">4</Label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="6" id="option-two" />
-              <Label htmlFor="option-two">6</Label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="8" id="option-two" />
-              <Label htmlFor="option-two">8</Label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="12" id="option-two" />
-              <Label htmlFor="option-two">12</Label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="16" id="option-two" />
-              <Label htmlFor="option-two">16</Label>
-            </div>
-          </RadioGroup>
-        </div>
-        <div className="flex flex-col gap-2">
-          <h3 className="text-base font-semibold flex flex-row justify-between">
-            Поиск
-          </h3>
-          <Input
-            type="text"
-            role="search"
-            id="q"
-            name="q"
-            placeholder="Поиск..."
-            onChange={(e) => setQ(e.target.value)}
-            value={q}
-          />
-        </div>
-        <Button
-          className="w-full"
-          type="reset"
-          onClick={() => {
-            navigate("/");
-          }}
-        >
-          Сбросить
-        </Button>
-      </Form>
-      <div className="p-4 flex flex-wrap gap-4">
-        {data.products.map((product) => (
-          <ProductCard
-            key={product.id}
-            {...(product as Omit<products, "created_at" | "updated_at">)}
-          />
-        ))}
       </div>
-    </div>
     </>
   );
 };
